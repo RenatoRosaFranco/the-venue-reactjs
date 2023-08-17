bb2976f8017844709dc96dad09760eefe6d7a30d
Renato Rosa Franco <renato_ny@live.com>
2023-08-17 19:31:40 -0300

    Header section added to the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { React } from 'react';
+import './resources/styles.css';
+
+import Header from './components/layouts/Header';
+// import Footer from './components/layouts/Footer';
+
+const App = () => {
+  return(
+    <div className='App'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App;
