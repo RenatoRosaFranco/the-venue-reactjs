a89b757f34a48082abea2d0bb36fbb780aa8f95e
Renato Rosa Franco <renato_ny@live.com>
2023-08-17 21:10:13 -0300

    Footer section added

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 const Footer = () => {
   return(
     <footer className="bck_red">
-      <div>
+      <Fade>
         <div className="font_righteous footer_logo_venue">The venue</div>
         <div className="footer_copyright">
           The venuw 2021. All rights reserved
         </div>
-      </div>
+      </Fade>
     </footer>
   )
 }
