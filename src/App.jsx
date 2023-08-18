65c419ca0cd604a37c37f1a770cfd8e794d7ad6d
Renato Rosa Franco <renato_ny@live.com>
2023-08-17 22:10:03 -0300

    Carrousel added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { React } from 'react';
 import './resources/styles.css';
 
 import Header from './components/layouts/Header';
+import Featured from './components/featured/Index';
 import Footer from './components/layouts/Footer';
 
 const App = () => {
   return(
     <div className='App'>
       <Header />
+
+      <>  
+        <Featured/>
+      </>
 
       <div style={{backgroundColor: 'red', height: '800px'}}></div>
       <div style={{backgroundColor: 'blue', height: '800px'}}></div>
